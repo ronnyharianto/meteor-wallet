7345d6387048bfd7d35466c4372ad635b590f32c
atlronny8047 <ronny.harianto@ai.astra.co.id>
2022-12-14 09:14:30 +0700

    Add styles

--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -1,59 +1,69 @@
-import React, { useState } from "react";
+import React from "react";
 import { ContactCollection } from "../api/ContactCollection";
 
 export const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [imageURL, setImageURL] = useState("");
-
-  const setValueName = (e) => {
-    setName(e.target.value);
-  };
-  const setValueEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const setValueImageURL = (e) => {
-    setImageURL(e.target.value);
-  };
-
-  const resetForm = () => {
-    setName("");
-    setEmail("");
-    setImageURL("");
-  };
+  const [name, setName] = React.useState(""); // Formik
+  const [email, setEmail] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
 
   const saveContact = () => {
-    console.log({ name, email, imageURL });
-
-    ContactCollection.insert({ name, email, imageURL });
-
-    resetForm();
-  };
+    ContactCollection.insert({ name, email, imageUrl });
+    setName("");
+    setEmail("");
+    setImageUrl("");
+  }
 
   return (
-    <form action="">
-      <div>
-        <label htmlFor="name">Name </label>
-        <input id="name" type="text" onChange={setValueName} value={name} />
+    <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="email">Email </label>
-        <input id="email" type="email" onChange={setValueEmail} value={email} />
-      </div>
-      <div>
-        <label htmlFor="imageUrl">Image </label>
-        <input
-          id="imageUrl"
-          type="text"
-          onChange={setValueImageURL}
-          value={imageURL}
-        />
-      </div>
-      <div>
-        <button type="button" onClick={saveContact}>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={saveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
           Save Contact
         </button>
       </div>
     </form>
-  );
-};
+  )
+}
--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,20 +1,34 @@
-import React from "react"
+import React from "react";
 import { ContactCollection } from "../api/ContactCollection";
 import { useTracker } from 'meteor/react-meteor-data';
 
 export const ContactList = () => {
     const contacts = useTracker(() => {
-        return ContactCollection.find({}).fetch();
+        return ContactCollection.find({}, { sort: { createdAt: -1 } }).fetch();
     });
 
     return (
-        <>
-            <h3>Contact List</h3>
-            <ul>
-                {contacts.map(contact => (
-                    <li key={contact._id}>{contact.name} - {contact.email}</li>
-                ))}
-            </ul>
-        </>
+        <div>
+            <div className="mt-10">
+                <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+                    Contact List
+                </h3>
+                <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+                    {contacts.map((person, personIdx) => (
+                        <li key={personIdx} className="py-4 flex items-center justify-between space-x-3">
+                            <div className="min-w-0 flex-1 flex items-center space-x-3">
+                                <div className="flex-shrink-0">
+                                    <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
+                                </div>
+                                <div className="min-w-0 flex-1">
+                                    <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
+                                    <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
